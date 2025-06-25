4f19d7d9014e02a4cdc323fee9c51f429fb58614
James Meegan <58824024+jmeegan2@users.noreply.github.com>
2025-06-24 20:10:43 -0500

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 // https://react.dev/learn/tutorial-tic-tac-toe
 // stopped here You will call calculateWinner(squares) in the Board component’s 
-
+//Implementing time travel 
 function Square({ value, onSquareClick }) { //value is the prop here 
   return <button className="square" onClick={onSquareClick}>
     {value}
@@ -13,12 +13,10 @@ function Square({ value, onSquareClick }) { //value is the prop here
 //shift + option + 'f' to format
 
 //default means its the main function in the file
-export default function Board() { //Board is the component
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null))
+function Board({ xIsNext, squares, onPlay }) { //Board is the component
 
   function handleClick(i) {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice() //we create a copy here because immutability
@@ -34,8 +32,7 @@ export default function Board() { //Board is the component
       nextSquares[i] = "O"
     }
 
-    setSquares(nextSquares)
-    setXIsNext(!xIsNext)
+    onPlay(nextSquares)
   }
 
   function calculateWinner(squares) {
@@ -57,10 +54,18 @@ export default function Board() { //Board is the component
     }
   }
 
+  const winner = calculateWinner(squares)
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O")
+  }
   // JSX is a combination of JavaScript code and html tags that describe what you'd like to display
   // (<> and </>) are fragments and u use it to wrap multiple adjacent JSX elements
   return (
     <>
+      <div className="status">{status}</div>
       <div className='board-row'>
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -79,6 +84,57 @@ export default function Board() { //Board is the component
       </div>
     </>
   )
+}
+
+/* default export is the convention if you want ot export only one object
+   there could be only one default export per file
+
+*/
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true)
+  const [history, setHistory] = useState([Array(9).fill(null)])
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    // TODO
+    setHistory([...history, nextSquares]);
+    /*
+    enumerate all the items in history
+
+    The spread (...) syntax allows an interable, such as an array or 
+    string to be expanded in places where zero or more arguments 
+    (for function calls) or elements (for array literals) are expected
+    */
+    setXIsNext(!xIsNext)
+  }
+
+  function jumpTo(nextMove) {
+    // TODO
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move; 
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    )
+  })
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
 }
 
 
